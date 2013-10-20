190a5fd45c18c21a2b4da7ec01d0145b200b3991
Geoffroy Aubry <gaubry@hi-media.com>
2013-10-21 00:46:28 +0200

    Add nounset and pipefail to bash scripts.

--- src/awk-csv-parser.sh
+++ src/awk-csv-parser.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# Treat unset variables and parameters other than the special parameters ‘@’ or ‘*’ as an error
+# when performing parameter expansion. An error message will be written to the standard error,
+# and a non-interactive shell will exit.
+set -o nounset
+
+# The return value of a pipeline is the value of the last (rightmost) command to exit with a non-zero status,
+# or zero if all commands in the pipeline exit successfully:
+set -o pipefail
+
 ROOT_DIR=$(cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/.." && pwd)
 
 separator=${1:-','}
