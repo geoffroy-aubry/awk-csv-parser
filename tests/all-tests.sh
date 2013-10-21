80fae1ecd18985cd93296d0b89c2ef6a2a24cee7
Geoffroy Aubry <gaubry@hi-media.com>
2013-10-21 09:26:23 +0200

    Add full test suite with summary.

--- tests/all-tests.sh
+++ tests/all-tests.sh
@@ -9,10 +9,27 @@ set -o nounset
 # or zero if all commands in the pipeline exit successfully:
 set -o pipefail
 
-TEST_DIR=$(cd "$(dirname "$(readlink -f "$BASH_SOURCE")")" && pwd)
-RESOURCES_DIR=$TEST_DIR/resources
+ROOT_DIR=$(cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/.." && pwd)
+SRC_DIR=$ROOT_DIR/src
+TESTS_DIR=$ROOT_DIR/tests
+RESOURCES_DIR=$TESTS_DIR/resources
 
-ok_path="$(mktemp /tmp/awk-csv-parser-XXXXXXXXXX)";
-cat tests/resources/ok.csv | src/awk-csv-parser.sh ',' '"' '|' > $ok_path
-diff --report-identical-files $ok_path $RESOURCES_DIR/ok-expected.txt
-# \ && …
+status=0
+for tests_set in ok invalid; do
+    echo -e "\033[1;37mTesting $RESOURCES_DIR/$tests_set.csv:\033[0m"
+    tmp_path="$(mktemp /tmp/awk-csv-parser-XXXXXXXXXX)";
+    cat $RESOURCES_DIR/$tests_set.csv \
+        | $SRC_DIR/awk-csv-parser.sh ',' '"' '|' \
+        | sed -r 's:(\033|\x1B)\[[0-9;]*[mK]::ig' \
+        > $tmp_path
+    diff --report-identical-files $tmp_path $RESOURCES_DIR/$tests_set-expected.txt
+    status=$(( $? | status))
+done
+
+echo
+if [ $status -ne 0 ]; then
+    echo -e "\033[1;37;41m\033[0K Tests FAILED!\033[0m\n"
+else
+    echo -e "\033[1;37;42m\033[0K Tests OK.\033[0m\n"
+fi
+exit $status
